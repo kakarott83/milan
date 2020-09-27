5165f8e82711bc1e5f0d2189b66b554a1ba92bd0
Michael Lange <lm28081983@gmail.com>
2020-09-27 11:10:16 +0200

    deleteSign-in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { flush } from '@angular/core/testing';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component'
+import { RegisterComponent } from './auth/register/register.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { StartComponent } from './start/start.component';
 
 
 const routes: Routes = [
   {path: 'start', component: StartComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'sign-in', component: SignInComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'reset-password', component: ResetPasswordComponent},
   {path: '', redirectTo: '/start', pathMatch: 'full'},
   {path: '**', component: StartComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AuthModule } from './auth/auth/auth.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 import { StartComponent } from './start/start.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignInComponent,
     ResetPasswordComponent,
-    StartComponent
+    StartComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
