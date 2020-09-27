45368ce9981e4c8162e47cd52d05c08373ea9ea8
Michael Lange <lm28081983@gmail.com>
2020-09-27 10:49:49 +0200

    addAuthComponents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
+import { flush } from '@angular/core/testing';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component'
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { StartComponent } from './start/start.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'start', component: StartComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
+  {path: '', redirectTo: '/start', pathMatch: 'full'},
+  {path: '**', component: StartComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AuthModule } from './auth/auth/auth.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignInComponent,
+    ResetPasswordComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
